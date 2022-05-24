7925dd14bb5ce5bc70c3cb6ac27e19b4dca7c3c4
JUAN PABLO ROJAS CONTRERAS <juanparojasc1999@gmail.com>
2022-05-23 20:00:41 -0600

    Agregar archivos de proyecto.

---
+++ monitoreo/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace monitoreo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.btn_Seleccionar = new System.Windows.Forms.Button();
+            this.txtArchivos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Filter = "*.* ";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher1_Renamed);
+            // 
+            // btn_Seleccionar
+            // 
+            this.btn_Seleccionar.Location = new System.Drawing.Point(603, 174);
+            this.btn_Seleccionar.Name = "btn_Seleccionar";
+            this.btn_Seleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Seleccionar.TabIndex = 0;
+            this.btn_Seleccionar.Text = "Seleccionar carpeta";
+            this.btn_Seleccionar.UseVisualStyleBackColor = true;
+            this.btn_Seleccionar.Click += new System.EventHandler(this.btn_Seleccionar_Click);
+            // 
+            // txtArchivos
+            // 
+            this.txtArchivos.Location = new System.Drawing.Point(34, 174);
+            this.txtArchivos.Multiline = true;
+            this.txtArchivos.Name = "txtArchivos";
+            this.txtArchivos.Size = new System.Drawing.Size(518, 150);
+            this.txtArchivos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Monitoreo De Archivos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtArchivos);
+            this.Controls.Add(this.btn_Seleccionar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.TextBox txtArchivos;
+        private System.Windows.Forms.Button btn_Seleccionar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ monitoreo/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace monitoreo
+{
+    public partial class Form1 : Form
+    {
+        string path = @"C:\Users\pablo\OneDrive\Documentos\prueba";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fileSystemWatcher1.Path = path;
+            GetFiles();
+        }
+        private void GetFiles()
+        {
+            String[] list = Directory.GetFiles(path);
+            txtArchivos.Text = "";
+            foreach (var sFile in list)
+            {
+                txtArchivos.Text += sFile + Environment.NewLine;
+            }
+        }
+
+        private void btn_Seleccionar_Click(object sender, EventArgs e)
+        {
+            GetFiles();
+            OpenFileDialog path= new OpenFileDialog();
+            path.Filter = "Todos los archivos|*.*";
+            path.Title = "Abrir...";
+             path.InitialDirectory = @"C:\";
+            if (path.ShowDialog() == DialogResult.OK)
+            {
+                if (txtArchivos.Text == "" || txtArchivos.Text == null) txtArchivos.Text = path.FileName;
+                else txtArchivos.Text += @"|" + path.FileName;
+            }
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            GetFiles();
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, RenamedEventArgs e)
+        {
+            GetFiles();
+        }
+    }
+}
